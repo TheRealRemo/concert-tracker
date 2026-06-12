b6c0cd474aa9fcb9e89bfdd31583f673b1c3673b
TheRealRemo <timothyprice2001@gmail.com>
2026-06-12 17:22:47 -0400

    Added constructors and getters and setters to venue entity

--- src/main/java/com/pluralsight/concerttracker/models/Venue.java
+++ src/main/java/com/pluralsight/concerttracker/models/Venue.java
@@ -16,4 +16,36 @@ public class Venue {
     private String city;
     private int capacity;
 
+    public Venue() {
+    }
+
+    public Venue(String name, String city, int capacity) {
+        this.name = name;
+        this.city = city;
+        this.capacity = capacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 }
