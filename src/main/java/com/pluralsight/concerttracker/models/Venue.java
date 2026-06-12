d37e85d4131f39c8c3b27f2f91ad9b4717ef1048
TheRealRemo <timothyprice2001@gmail.com>
2026-06-12 17:21:04 -0400

    Added relevant entities, services, and repositories, updated venue with fields

---
+++ src/main/java/com/pluralsight/concerttracker/models/Venue.java
@@ -0,0 +1,19 @@
+package com.pluralsight.concerttracker.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Venue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String city;
+    private int capacity;
+
+}
