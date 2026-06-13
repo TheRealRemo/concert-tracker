a9dd0e8d5574cf402e380152268291448a34408d
TheRealRemo <timothyprice2001@gmail.com>
2026-06-12 23:34:40 -0400

    Added fields, constructor, getters and setters to 'Concert' class

--- src/main/java/com/pluralsight/concerttracker/models/Concert.java
+++ src/main/java/com/pluralsight/concerttracker/models/Concert.java
@@ -7,8 +7,52 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Concert {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private int year;
+    private double ticketPrice;
+    private int ticketsSold;
 
+    public Concert() {
+    }
 
+    public Concert(int year, double ticketPrice, int ticketsSold) {
+        this.year = year;
+        this.ticketPrice = ticketPrice;
+        this.ticketsSold = ticketsSold;
+    }
 
+    public double getTicketPrice() {
+        return ticketPrice;
+    }
+
+    public void setTicketPrice(double ticketPrice) {
+        this.ticketPrice = ticketPrice;
+    }
+
+    public int getTicketsSold() {
+        return ticketsSold;
+    }
+
+    public void setTicketsSold(int ticketsSold) {
+        this.ticketsSold = ticketsSold;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
