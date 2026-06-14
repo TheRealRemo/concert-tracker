0e941cd0d4889f8aa7fe1e4da422ccf2e4a3350a
TheRealRemo <timothyprice2001@gmail.com>
2026-06-13 22:38:11 -0400

    updated seed for mySQL starter data

--- src/main/java/com/pluralsight/concerttracker/service/ConcertService.java
+++ src/main/java/com/pluralsight/concerttracker/service/ConcertService.java
@@ -5,6 +5,7 @@ import com.pluralsight.concerttracker.data.ConcertRepository;
 import com.pluralsight.concerttracker.data.PromoterRepository;
 import com.pluralsight.concerttracker.data.VenueRepository;
 import com.pluralsight.concerttracker.models.Artist;
+import com.pluralsight.concerttracker.models.Promoter;
 import com.pluralsight.concerttracker.models.Venue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -34,6 +35,13 @@ public class ConcertService {
         Artist ytg = artistRepository.save(new Artist("Young The Giant", "Alternative Rock"));
         Artist beabadoobee = artistRepository.save(new Artist("beabadoobee", "Alternative Rock"));
         Artist mj = artistRepository.save(new Artist("Michael Jackson", "Pop"));
-        Venue venue = venueRepository.save(new Venue("Metlife Stadium", "East Rutherford", 82500));
+        Venue metlifeStadium = venueRepository.save(new Venue("Metlife Stadium", "East Rutherford", 82500));
+        Venue madisonSquareGarden = venueRepository.save(new Venue("Madison Square Garden", "New York", 19800));
+        Venue wellsFargo = venueRepository.save(new Venue("Wells Fargo Center", "Philadelphia", 21000));
+        Promoter liveNation = promoterRepository.save(new Promoter("Live Nation"));
+        Promoter aeg = promoterRepository.save(new Promoter("AEG Presents"));
+        Promoter c3 = promoterRepository.save(new Promoter("C3 Presents"));
+
+
     }
 }
--- src/main/java/com/pluralsight/concerttracker/ui/ConcertUserInterface.java
+++ src/main/java/com/pluralsight/concerttracker/ui/ConcertUserInterface.java
@@ -1,7 +1,8 @@
-package ui;
+package com.pluralsight.concerttracker.ui;
 
 import com.pluralsight.concerttracker.models.Concert;
 import com.pluralsight.concerttracker.service.ConcertService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
 public class ConcertUserInterface implements CommandLineRunner {
    private final ConcertService concertService;
 
+   @Autowired
     public ConcertUserInterface(ConcertService concertService) {
         this.concertService = concertService;
     }
